1116acf20b1a20f276ec61312e327a59e4135c5e
jona881a <jonaskunert@hotmail.com>
2021-11-23 12:26:27 +0100

    Fixed projektet så vi kan få en assignment tabel ned i databasen

--- src/main/java/com/workcalendar/caldenar/model/Assignment.java
+++ src/main/java/com/workcalendar/caldenar/model/Assignment.java
@@ -1,4 +1,29 @@
 package com.workcalendar.caldenar.model;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+
+@Getter @Setter @NoArgsConstructor
+@Entity
 public class Assignment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer assignmentID;
+    private String address;
+    private Time assignmentStartTime;
+    private Time assignmentEndTime;
+    private Date assignmentStartDate;
+    private Date assignmentEndDate;
+    private String description;
+    private String color;
+
 }
--- src/main/java/com/workcalendar/caldenar/repository/AssignmentRepository.java
+++ src/main/java/com/workcalendar/caldenar/repository/AssignmentRepository.java
@@ -3,5 +3,5 @@ package com.workcalendar.caldenar.repository;
 import com.workcalendar.caldenar.model.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssignmentRepsitory extends JpaRepository<Integer, Assignment> {
+public interface AssignmentRepository extends JpaRepository<Assignment, Integer> {
 }
