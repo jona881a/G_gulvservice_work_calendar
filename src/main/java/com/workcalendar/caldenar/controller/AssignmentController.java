6194ff8dbd2549486fe1b3ea5a431b08d0593a58
jona881a <jonaskunert@hotmail.com>
2021-11-23 12:45:57 +0100

    Lavet Postcontrolleren for assignment

--- src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
+++ src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
@@ -1,4 +1,22 @@
 package com.workcalendar.caldenar.controller;
 
+import com.workcalendar.caldenar.model.Assignment;
+import com.workcalendar.caldenar.repository.AssignmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 public class AssignmentController {
+
+    @Autowired
+    AssignmentRepository assignmentRepository;
+
+    @PostMapping(value="/assignment", consumes = "application/json")
+    public ResponseEntity<Assignment> postAssignment(@RequestBody Assignment assignment) {
+        assignmentRepository.save(assignment);
+
+        return new ResponseEntity<>(assignment, HttpStatus.CREATED);
+    }
 }
