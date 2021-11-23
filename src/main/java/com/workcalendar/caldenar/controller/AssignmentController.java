75963217e25f1cb0fb4d7812390b0f455a9c60a0
jona881a <jonaskunert@hotmail.com>
2021-11-23 12:55:48 +0100

    Lavet deletemapping til assignment

--- src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
+++ src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
@@ -3,8 +3,11 @@ package com.workcalendar.caldenar.controller;
 import com.workcalendar.caldenar.model.Assignment;
 import com.workcalendar.caldenar.repository.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.spel.ast.Assign;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -18,5 +21,18 @@ public class AssignmentController {
         assignmentRepository.save(assignment);
 
         return new ResponseEntity<>(assignment, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/assignment/{id}")
+    public ResponseEntity<Object> deleteAssignment(@PathVariable int id) {
+
+        try {
+            assignmentRepository.deleteById(id);
+
+        } catch (Exception err) {
+
+            return new ResponseEntity<>("assignment with specified id: " + id + " not found", HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
