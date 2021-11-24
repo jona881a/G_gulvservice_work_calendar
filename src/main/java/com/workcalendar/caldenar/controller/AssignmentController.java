3469ba64eb8870c7c1db91f56ec61a1bb32aed10
jona881a <jonaskunert@hotmail.com>
2021-11-24 11:27:24 +0100

    Fixed cross origin, vi får POST 400 bad request fejl

--- src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
+++ src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
@@ -6,11 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.expression.spel.ast.Assign;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin("*")
+@RestController
 public class AssignmentController {
 
     @Autowired
