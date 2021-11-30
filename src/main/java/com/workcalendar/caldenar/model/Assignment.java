976d418e5a7933f297723e863a80484043e45864
Raskoe99 <r.koefoed@hotmail.com>
2021-11-30 10:54:52 +0100

    Assignment attributes now work

--- src/main/java/com/workcalendar/caldenar/model/Assignment.java
+++ src/main/java/com/workcalendar/caldenar/model/Assignment.java
@@ -16,8 +16,8 @@ public class Assignment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String title;
     private Integer assignmentID;
+    private String title;
     private String address;
     private Date assignmentStartDateTime;
     private Date assignmentEndDateTime;
