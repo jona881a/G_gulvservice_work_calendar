ae6618633f2aa844aef1b1215c98738062b4d138
jona881a <jonaskunert@hotmail.com>
2022-02-02 10:05:31 +0100

    Added build.yml file to GithubActions

--- src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
+++ src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
@@ -4,7 +4,6 @@ import com.workcalendar.caldenar.exceptions.ResourceNotFoundException;
 import com.workcalendar.caldenar.model.Assignment;
 import com.workcalendar.caldenar.repository.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.expression.spel.ast.Assign;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -48,17 +47,41 @@ public class AssignmentController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
-    @PutMapping("/assignment/{id}")
+    @PutMapping("/assignment/{assignmentID}")
     public ResponseEntity<Assignment> updateAssignment(@PathVariable int assignmentID, @RequestBody Assignment assignment) {
-        System.out.println(assignmentID);
+
         Optional<Assignment> assignmentDetails = assignmentRepository.findById(assignmentID);
+
         if (assignmentDetails.isPresent()) {
             Assignment _assignment = assignmentDetails.get();
-            _assignment.setAssignmentID(assignment.getAssignmentID());
+            _assignment.setTitle(assignment.getTitle());
+            _assignment.setAddress(assignment.getAddress());
+            _assignment.setAssignmentStartDateTime(assignment.getAssignmentStartDateTime());
+            _assignment.setAssignmentEndDateTime(assignment.getAssignmentEndDateTime());
+            _assignment.setDescription(assignment.getDescription());
+            _assignment.setColor(assignment.getColor());
+
             _assignment = assignmentRepository.save(_assignment);
             return new ResponseEntity<>(_assignment, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    /*
+    @PutMapping("/kommune/{kommuneKode}")
+    public ResponseEntity<Kommune> updateKommune(@PathVariable int kommuneKode, @RequestBody Kommune kommune) {
+        Optional<Kommune> kommuneData = kommuneRepository.findById(kommuneKode);
+
+        if (kommuneData.isPresent()) {
+            Kommune _kommune = kommuneData.get();
+            _kommune.setKommuneNavn(kommune.getKommuneNavn()); //her sættes kommunenavn
+            _kommune = kommuneRepository.save(_kommune);
+            return new ResponseEntity<>(_kommune, HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+     */
 }
