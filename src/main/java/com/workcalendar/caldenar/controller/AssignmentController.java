0d34b4dfac2817e3dd74d86c848130aa6bc4c792
jona881a <jonaskunert@hotmail.com>
2022-02-22 10:53:15 +0100

    Ændret yml fil

--- src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
+++ src/main/java/com/workcalendar/caldenar/controller/AssignmentController.java
@@ -1,15 +1,12 @@
 package com.workcalendar.caldenar.controller;
 
-import com.workcalendar.caldenar.exceptions.ResourceNotFoundException;
 import com.workcalendar.caldenar.model.Assignment;
 import com.workcalendar.caldenar.repository.AssignmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -67,21 +64,4 @@ public class AssignmentController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
-
-    /*
-    @PutMapping("/kommune/{kommuneKode}")
-    public ResponseEntity<Kommune> updateKommune(@PathVariable int kommuneKode, @RequestBody Kommune kommune) {
-        Optional<Kommune> kommuneData = kommuneRepository.findById(kommuneKode);
-
-        if (kommuneData.isPresent()) {
-            Kommune _kommune = kommuneData.get();
-            _kommune.setKommuneNavn(kommune.getKommuneNavn()); //her sættes kommunenavn
-            _kommune = kommuneRepository.save(_kommune);
-            return new ResponseEntity<>(_kommune, HttpStatus.OK);
-        } else {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-        }
-    }
-
-     */
 }
